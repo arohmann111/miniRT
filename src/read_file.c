6ec322e0d77dea0a4cab58aae4f1d103c1453d1b
Alisja Frasch <afrasch@1-A-1.42heilbronn.de>
2022-05-09 14:31:39 +0200

    nur object cylinder fehlt

--- src/read_file.c
+++ src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: afrasch <afrasch@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 11:23:22 by afrasch           #+#    #+#             */
-/*   Updated: 2022/05/06 17:24:33 by afrasch          ###   ########.fr       */
+/*   Updated: 2022/05/09 14:30:21 by afrasch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int	print_error(char *err_msg)
 bool	check_range(double range_start, double range_end, double val)
 {
 	if (!(val >= range_start && val <= range_end))
-	{
-		print_error("value not in range\n");
 		return (false);
-	}
 	return (true);
 }
 
@@ -62,7 +59,7 @@ int	get_colors(t_colors *c, char *split_str)
 		if (error == ERROR)
 			return (print_error("Color can't be converted\n"));
 		if (check_range(0.0, 255.0, (double)rgb[i]) == false)
-			return(print_error("Color is not in range\n"));
+			return(print_error("Color is not in range [0-255]\n"));
 		i++;
 	}
 	c->r = rgb[0];
@@ -95,12 +92,12 @@ int	get_vector(t_vec3d *v, char *split_str, int type)
 		if (type == COORDINATES)
 		{
 			if (check_range(-500.0, 500.0, xyz[i]) == false)
-				return(print_error("Coordinate vector is not in range\n"));
+				return(print_error("Coordinate vector is not in range [-500.0,500.0]\n"));
 		}
 		else if (type == ORIENTATION)
 		{
 			if (check_range(-1.0, 1.0, xyz[i]) == false)
-				return(print_error("Orientation vector is not in range\n"));
+				return(print_error("Orientation vector is not in range [-1.0,1.0]\n"));
 		}
 		i++;
 	}
@@ -124,7 +121,7 @@ int get_ambiente(t_scene *scene, char **split)
 	if (error == ERROR)
 		return (print_error("Ambient light ratio can't be converted\n"));
 	if (check_range(0.0, 1.0, ratio) == false)
-		return (print_error("Ambient light ratio is not in range\n"));
+		return (print_error("Ambient light ratio is not in range [0.0,1.0]\n"));
 	scene->ambiente.ratio = ratio;
 	if (get_colors(&scene->ambiente.colors, split[2]) == ERROR)
 		return (ERROR);
@@ -151,7 +148,7 @@ int get_camera(t_scene *scene, char **split)
 	if (error == ERROR)
 		return (print_error("Field of view can't be converted\n"));
 	if (check_range(0.0, 180.0, (double)field_of_view) == false)
-		return (print_error("Field of view is not in range\n"));
+		return (print_error("Field of view is not in range [0-180]\n"));
 	scene->camera.fov = field_of_view;
 	scene->camera.is_set = true;
 	return (0);
@@ -174,7 +171,7 @@ int	get_light(t_scene *scene, char **split)
 	if (error == ERROR)
 		return (print_error("Brightness ratio can't be converted\n"));
 	if (check_range(0.0, 1.0, (double)brightness_ratio) == false)
-		return (print_error("Brightness ratio is not in range\n"));
+		return (print_error("Brightness ratio is not in range [0.0,1.0]\n"));
 	//range in print_error angeben
 	scene->light.bright = brightness_ratio;
 	if (get_colors(&scene->light.colors, split[3]) == ERROR)
@@ -207,24 +204,68 @@ int	get_resolution(t_scene *scene, char **split)
 	return (0);
 }
 
+t_list	*get_new_obj(int type)
+{
+	t_object	*obj;
+
+	obj = malloc(sizeof(t_object) * 1);
+	if (!obj)
+		return (NULL);
+	obj->type = type;
+	return (ft_lstnew(obj));
+}
+
 int get_plane(t_scene *scene, char **split)
 {
 	t_list		*new_elem;
-	t_object	*obj;
 
 	if (arrlen(split) != 4)
 		return (print_error("Wrong plane input\n"));
-	new_elem = ft_lstnew(obj);
+	new_elem = get_new_obj(PLANE);
+	if (!new_elem)
+		return (print_error("Plane obj can't be allocated\n"));
 	ft_lstadd_back(&scene->list, new_elem);
-	
+	// printf("check in function %s\n", __func__);
+	if (get_vector(&((t_object*)new_elem->content)->pos, split[1], COORDINATES) == ERROR)
+		return (ERROR);
+	if (get_vector(&((t_object*)new_elem->content)->pl.orient, split[2], ORIENTATION) == ERROR)
+		return (ERROR);
+	if (get_colors(&((t_object*)new_elem->content)->colors, split[3]) == ERROR)
+		return (ERROR);
+	return (0);
 }
+
+int get_sphere(t_scene *scene, char **split)
+{
+	t_list	*new;
+	double	dia;
+	int		error;
+
+	if (arrlen(split) != 4)
+		return (print_error("Wrong sphere input\n"));
+	new = get_new_obj(SPHERE);
+	if (!new)
+		return (print_error("Sphere object can't be allocated\n"));
+	ft_lstadd_back(&scene->list, new);
+	if (get_vector(&((t_object*)new->content)->pos, split[1], COORDINATES) == ERROR)
+		return (ERROR);
+	dia = ft_atod(split[2], &error);
+	if (error == ERROR)
+		return (print_error("Sphere diameter can't be converted\n"));
+	((t_object*)new->content)->sp.diameter = dia;
+	if (get_colors(&((t_object*)new->content)->colors, split[3]) == ERROR)
+		return (ERROR);
+	return (0);
+}
+
+int	
 
 int	get_obj(t_scene *scene, char **split)
 {
 	if (split[0][0] == 'p' && split[0][1] == 'l')
 		return (get_plane(scene, split));
-	// if (split[0][0] == 's' && split[0][1] == 'p')
-	// 	return (get_sphere(scene, split));
+	if (split[0][0] == 's' && split[0][1] == 'p')
+		return (get_sphere(scene, split));
 	// if (split[0][0] == 'c' && split[0][1] == 'y')
 	// 	return (get_cylinder(scene, split));
 	else
