82e970437a95c0730594b21e8e075b0497add0fc
Alisja Frasch <afrasch@1-A-1.42heilbronn.de>
2022-05-16 13:49:49 +0200

    bla

--- src/trace_rays.c
+++ src/trace_rays.c
@@ -1,3 +1,5 @@
+
+
 #include "miniRT.h"
 
 int	find_inter(t_scene *scene, t_vec3d dir)
@@ -15,22 +17,23 @@ int	find_inter(t_scene *scene, t_vec3d dir)
 	c = skalar_vec3d(sub_vec3d(scene->camera.pos, sphere->pos),
 		sub_vec3d(scene->camera.pos, sphere->pos)) - sphere->sp.diameter * sphere->sp.diameter / 4.0;
 	dis = b*b - 4.0*a*c;
-	if ( dis < 0)
-		return (-1.0);
+	if (dis < 0)
+		return (ERROR);//keine Lösung -> kein Schnittpunkt
 	else
-		return (-b - sqrt(dis) / (2.0 * a));
+		return (-b - sqrt(dis) / (2.0 * a));//Mitternachtsformel
 }
 
 int	trace(t_scene *scene, t_vec3d dir)
 {
-	int t;
-	t_vec3d n;
+	int t;//für v = campos + t*camdir
+	t_vec3d n;//normale
 
 	t = find_inter(scene, dir);
-	if ( t > 0.0)
+	if ( t > 0.0)//Punkt befindet sich im Blickfeld
 	{
 		// n = norm_vec3d(add_vec3d(scene->camera.pos, sub_vec3d(multi_vec3d(dir, t), ((t_object *)(scene->list->content))->pos)));
 		n = norm_vec3d(sub_vec3d(add_vec3d(scene->camera.pos, multi_vec3d(dir, t)), ((t_object *)(scene->list->content))->pos));
+		//
 		return (col((n.x - 1.0) / -2.0 * 255.0, (n.y - 1.0) / -2.0 * 255.0, (n.z - 1.0) / -2.0 * 255.0));
 	}
 	return (col((dir.x + 1.0) / 2.0 * 255.0, (dir.y + 1.0) / 2.0 * 255.0, (dir.z + 1.0) / 2.0 * 255.0));
