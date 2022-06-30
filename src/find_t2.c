d83b8a87b269e3a1b065a61aec8878e8b4758da9
arohmann <arohmann@student.42heilbronn.de>
2022-06-30 10:34:00 +0200

    find_t2 normed

--- src/find_t2.c
+++ src/find_t2.c
@@ -3,54 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   find_t2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afrasch <afrasch@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: arohmann <arohmann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:07:52 by arohmann          #+#    #+#             */
-/*   Updated: 2022/06/29 19:08:07 by afrasch          ###   ########.fr       */
+/*   Updated: 2022/06/30 10:27:18 by arohmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+double	tube_t2(t_object *tube, t_ray ray, double t)
+{
+	t_vec3d	wtf;
+	t_vec3d	p;
+	double	cut;
 
+	p = add_vec3d(ray.pos, multi_vec3d(ray.dir, t));
+	wtf = sub_vec3d(p, tube->pos);
+	cut = skalar_vec3d(tube->tb.orient, wtf);
+	return (cut);
+}
 
 double	tube_find_t(t_object *tube, t_ray ray)
 {
-	double	a;
-	double	b;
-	double	c;
+	double	m[3];
 	double	dis;
-	double	cut;
-	double	t;
+	double	t[2];
 	t_vec3d	tmp;
 	t_vec3d	tmp1;
-	t_vec3d	wtf;
-	t_vec3d	p;
 
 	tmp = cross_vec3d(ray.dir, tube->tb.orient);
 	tmp1 = cross_vec3d(sub_vec3d(ray.pos, tube->pos), tube->tb.orient);
-	a = skalar_vec3d(tmp, tmp);
-	b = 2.0 * skalar_vec3d(tmp, tmp1);
-	c = skalar_vec3d(tmp1, tmp1) - pow(tube->tb.diameter / 2.0, 2);
-	
-	dis = b * b - 4.0 * a * c;
+	m[0] = skalar_vec3d(tmp, tmp);
+	m[1] = 2.0 * skalar_vec3d(tmp, tmp1);
+	m[2] = skalar_vec3d(tmp1, tmp1) - pow(tube->tb.diameter / 2.0, 2);
+	dis = m[1] * m[1] - 4.0 * m[0] * m[2];
 	if (dis < 0.0)
 		return (-1.0);
-
-	t = (-b - sqrt(dis)) / (2.0 * a);
-
-	p = add_vec3d(ray.pos, multi_vec3d(ray.dir, t));
-	wtf = sub_vec3d(p, tube->pos);
-	cut = skalar_vec3d(tube->tb.orient, wtf);
-	if (t > 0.0001 && fabs(cut) < (tube->tb.height / 2.0))
-		return (t);
-
-	t = (-b + sqrt(dis)) / (2.0 * a);
-
-	p = add_vec3d(ray.pos, multi_vec3d(ray.dir, t));
-	wtf = sub_vec3d(p, tube->pos);
-	cut = skalar_vec3d(tube->tb.orient, wtf);
-	if (t > 0.0001 && fabs(cut) < (tube->tb.height / 2.0))
-		return (t);
+	t[0] = (-m[1] - sqrt(dis)) / (2.0 * m[0]);
+	t[1] = tube_t2(tube, ray, t[0]);
+	if (t[0] > 0.0001 && fabs(t[1]) < (tube->tb.height / 2.0))
+		return (t[0]);
+	t[0] = (-m[1] + sqrt(dis)) / (2.0 * m[0]);
+	t[1] = tube_t2(tube, ray, t[0]);
+	if (t[0] > 0.0001 && fabs(t[1]) < (tube->tb.height / 2.0))
+		return (t[0]);
 	return (-1.0);
 }
